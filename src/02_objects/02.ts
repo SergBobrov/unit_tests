1e12ec642a8d3cc0cdad66bab5cc82969e4bb91b
Serg <serggbobrov@gmail.com>
2020-08-19 13:45:01 +0300

    object typification

---
+++ src/02_objects/02.ts
@@ -0,0 +1,55 @@
+type CityType = {
+    title: string;
+    countryTitle: string;
+}
+type addressType = {
+    streetTitle: string;
+    city: CityType;
+}
+type TechType = {
+    id: number
+    title: string
+}
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    address: addressType
+    isActive: boolean
+    technologies: Array<TechType>
+}
+
+const student: StudentType = {
+    id: 1,
+    name: "Dimych",
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: "Surganova 2",
+        city: {
+            title: "Minsk",
+            countryTitle: "Belarus"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "React"
+        },
+    ]
+}
+
+console.log(student.age);
+console.log(student.name);
+
+console.log(student.address.city.title);
+
+console.log(student.technologies[2].title);
